1d08003fa5392e8c33386f5b816ba94f476b429d
Filipe Gama <filipegama715@gmail.com>
2021-09-16 14:41:32 -0300

    Criando evento no formulário

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTranferenciaComponent {}
+export class NovaTranferenciaComponent {
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+  }
+}
